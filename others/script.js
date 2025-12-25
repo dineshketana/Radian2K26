fdc5cbc714b2ef397ac7a41e13b4706d79402263
dineshketana <dineshketana@gmail.com>
2025-12-25 18:50:11 +0530

    Files

--- others/script.js
+++
@@ -1,9 +0,0 @@
-// subtle glow interaction
-document.querySelectorAll(".event-card").forEach(card => {
-  card.addEventListener("mouseenter", () => {
-    card.style.boxShadow += ", 0 0 25px white";
-  });
-  card.addEventListener("mouseleave", () => {
-    card.style.boxShadow = "";
-  });
-});
